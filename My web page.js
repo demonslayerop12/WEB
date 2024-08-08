1f677799d2cb685a76b84cbe743ff6a5a3947cd4
demonslayerop12 <muhammad033412@gmail.com>
2024-08-08 15:25:17 +0500

    Add files via upload

---
+++ My web page.js
@@ -0,0 +1,34 @@
+const Rengoku = document.getElementById("renGoku");
+console.log(Rengoku)
+Rengoku.addEventListener("click",function(e) {
+    console.log("The button was clicked")
+});
+const shinazugawa = document.getElementById("shinaZugawa");
+console.log(shinazugawa)
+shinazugawa.addEventListener("click",function(e){console.log("The button was clicked")});
+
+const Muchiro = document.getElementById("mucHiro");
+console.log(Muchiro)
+Muchiro.addEventListener("click",function(e){console.log("The button was clicked")});
+
+const Gyomei = document.getElementById("gyoMei");
+console.log(Gyomei)
+Gyomei.addEventListener("click",function(e){console.log("The button was clicked")});
+
+const Giyu = document.getElementById("giYu")
+console.log(Giyu)
+Giyu.addEventListener("click",function(e){console.log("The button was clicked")});
+
+const Obanai = document.getElementById("obaNai");
+console.log(Obanai)
+Obanai.addEventListener("click",function(e){console.log("The button was clicked")});
+
+const Mitsuri = document.getElementById("mitSuri");
+console.log(Mitsuri)
+Mitsuri.addEventListener("click",function(e){console.log("The button was clicked")});
+
+const Tengen = document.getElementById("tenGen")
+console.log(Tengen)
+Tengen.addEventListener("click",function(e){console.log("The button was clicked")})
+
+// Variales can not be reserved keyword 
